065d525f6531b8e6d715cf121967622dd46328bd
Juan Camilo <juandesigner95@gmail.com>
2019-10-21 18:08:25 -0500

    cambios de produccion

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -11,9 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    this.router.navigate(['login']);
   }
 
 }
